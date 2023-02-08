3c5e939f5349aad15777eb70d5c8bae4664a8b80
Paul Downey <psd@whatfettle.com>
2023-02-08 11:45:13 +0000

    Code to seed files from collections

---
+++ bin/download-pipeline.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+github="https://raw.githubusercontent.com/digital-land/"
+
+csvcut -c collection specification/collection.csv | tail -n +2 |
+while read collection
+do
+    for file in \
+	column.csv\
+	combine.csv\
+	concat.csv\
+	convert.csv\
+	default.csv\
+	default-value.csv\
+	patch.csv\
+	skip.csv\
+	transform.csv\
+	filter.csv\
+	lookup.csv
+    do
+        dir=pipeline/$collection
+        path=pipeline/$collection/$file
+        if [ ! -f $path ] ; then
+            mkdir -p $dir
+            set -x
+            wget -q -O $path "$github$collection-collection/main/pipeline/$file"
+            set +x
+        fi
+    done
+done
+
